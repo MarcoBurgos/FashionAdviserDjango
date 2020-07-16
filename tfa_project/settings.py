239f32e6bd3221b8682a025fae0279822a937261
marcoburgos <marko_burgos@gmail.com>
2020-07-16 00:57:11 -0400

    settings psql

--- tfa_project/settings.py
+++ tfa_project/settings.py
@@ -79,24 +79,24 @@ WSGI_APPLICATION = 'tfa_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'database_name',
-#         'USER': 'database_user',
-#         'PASSWORD': 'database_password',
-#         'HOST': 'database_endpoint',
-#         'PORT': 'database_port',
-#
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tfa_db',
+        'USER': 'postgres',
+        'PASSWORD': 'admin2124',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
