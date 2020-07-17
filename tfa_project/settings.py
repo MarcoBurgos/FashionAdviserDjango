ba9dbfe681ea20c0d764382bbd303e5cb441d1c8
marcoburgos <marko_burgos@gmail.com>
2020-07-16 21:32:49 -0400

    whitenoise config

--- tfa_project/settings.py
+++ tfa_project/settings.py
@@ -143,8 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/images/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
