44f40d81a8ec78510358f829b598a3d331906129
marcoburgos <marko_burgos@gmail.com>
2020-07-16 21:46:24 -0400

    change db

--- tfa_project/settings.py
+++ tfa_project/settings.py
@@ -98,13 +98,13 @@ DATABASES = {
 #     }
 # }
 
-# import dj_database_url
-# from decouple import config
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
+import dj_database_url
+from decouple import config
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 # Password validation
